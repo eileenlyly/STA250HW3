29970e1752f36386e5c6e28eb9f3ac2ad745c02d
eileenlyly <eileenlyly@gmail.com>
2014-02-25 15:37:26 -0800

    code update

--- CategorizeTags.py
+++ CategorizeTags.py
@@ -1,8 +1,11 @@
 import csv
+import os
 
-reader = csv.reader( open('/Users/eileenlyly/courses/STA250/HW3/tags_description_5000.csv'))
+os.chdir('/Users/eileenlyly/courses/STA250/HW3/')
 
-with open('/Users/eileenlyly/courses/STA250/HW3/tag_lng.csv','w') as output1, open('/Users/eileenlyly/courses/STA250/HW3/tag_lib.csv','w') as output2, open('/Users/eileenlyly/courses/STA250/HW3/tag_app.csv','w') as output3:
+reader = csv.reader( open('tags_description_5000.csv'))
+
+with open('tag_lng.csv','w') as output1, open('tag_lib.csv','w') as output2, open('tag_app.csv','w') as output3:
     for tag in reader:
         if "language" in tag[1]:
             output1.write(tag[0])
