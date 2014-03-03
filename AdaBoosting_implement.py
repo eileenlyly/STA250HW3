5ca816c760838baec09724d5b4ac792c5efe49c3
eileenlyly <eileenlyly@gmail.com>
2014-03-02 17:01:45 -0800

    implement adaboosting

---
+++ AdaBoosting_implement.py
@@ -0,0 +1,90 @@
+import os, math
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score
+import numpy as np
+from numpy.random import rand
+
+def sampleByWeight(array, weights, size):
+    index = []
+    maxidx = len(array)
+    cs = np.cumsum(weights)
+    while len(index) < size:
+        index.append(sum(cs < rand()))
+    
+    return array[index]
+
+class AdaBoosting:
+    
+    def __init__(self,classifier):
+        self.classifier = classifier
+        self.weak_classifier_ensemble = []
+        self.alpha = []
+    
+    def train(self, X, Y, T, k):
+        n = len(X)
+        w = (1.0/n) * np.ones(n) 
+        self.T = T
+        self.weak_classifier_ensemble = []
+        self.alpha = []     
+        for t in range(T):
+            weak_learner = self.classifier()
+            x_sample = sampleByWeight(X,w,k)
+            y_sample = sampleByWeight(Y,w,k)
+            weak_learner.fit(x_sample,y_sample)
+            Y_pdkt = weak_learner.predict(X)
+            e = np.zeros(n)
+            error = 0
+            for i in range(n):
+                if Y_pdkt[i] != Y[i]:
+                    e[i] = 1
+                    error += 1
+                    
+            error = error * 1.0 / n            
+            alpha = math.log((1 - error) / error)
+            w *= np.exp(alpha * e)
+            w /= sum(w)
+            self.weak_classifier_ensemble.append(weak_learner)
+            self.alpha.append(alpha) 
+        
+        
+    def predict(self, X, maxY):
+        n = len(X)
+        Y = np.zeros(n)
+        e = sum(self.alpha)
+        for t in range(self.T):
+            weak_learner = self.weak_classifier_ensemble[t]
+            Y += weak_learner.predict(X) * self.alpha[t] / e
+            
+        for i in range(len(Y)):
+            Y[i] = min(int(round(Y[i])), maxY)
+                
+        return Y
+        
+        
+if __name__ == "__main__":
+        
+    os.chdir('/Users/eileenlyly/courses/STA250/HW3/data/')
+
+    train_data = np.loadtxt('train-output.csv',dtype=np.object,delimiter=',')
+    train_data = train_data[1:,:].astype(np.int)
+
+    pdkt_data = np.loadtxt('pdkt-output.csv',dtype=np.object,delimiter=',')
+    pdkt_data = pdkt_data[1:,:].astype(np.int)
+    pdkt_input = pdkt_data[:,1:]
+    pdkt_true = np.ravel(pdkt_data[:,0:1])
+
+    classes = np.ravel(train_data[:,0:1])
+    predictors = train_data[:,1:]
+        
+    ab = AdaBoosting(GaussianNB)
+    T = 10
+    k = max(10, len(predictors) / 1000)
+    ab.train(predictors, classes, T, k)
+    pdkt_res = ab.predict(pdkt_input,5)
+    print(pdkt_res)
+    acc_score = accuracy_score(pdkt_true,pdkt_res)
+    prc_score = precision_score(pdkt_true,pdkt_res)
+
+    print(acc_score)
+    print(prc_score)
+    
